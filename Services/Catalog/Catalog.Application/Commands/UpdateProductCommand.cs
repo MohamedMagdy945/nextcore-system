7813d24acbf621efd77e455106b472da751cb308
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-07 21:12:02 +0200

    feat: add catalog update product handler

---
+++ Services/Catalog/Catalog.Application/Commands/UpdateProductCommand.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.Core.Entities;
+using MediatR;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Application.Commands
+{
+    public class UpdateProductCommand : IRequest<bool>
+    {
+
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+        public decimal Price { get; set; }
+
+        public ProductBrand Brand { get; set; } = default!;
+        public ProductType Type { get; set; } = default!;
+
+        public string ImageFile { get; set; } = string.Empty;
+    }
+}
---
+++ Services/Catalog/Catalog.Application/Handlers/Commands/UpdateProductCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Catalog.Application.Commands;
+using Catalog.Application.Interfaces.Repositories;
+using Catalog.Core.Entities;
+using MediatR;
+
+namespace Catalog.Application.Handlers.Commands
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, bool>
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _productRepository;
+        public UpdateProductCommandHandler(
+            IMapper mapper, IProductRepository
+            productRepository)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public async Task<bool> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var productEntity = new Product()
+            {
+                Id = request.Id,
+                Name = request.Name,
+                Description = request.Description,
+                ImageFile = request.ImageFile,
+                Price = request.Price,
+                Summary = request.Summary,
+                Brand = request.Brand,
+                Type = request.Type,
+            };
+            var product = await _productRepository.UpdateAsync(productEntity);
+            return product;
+        }
+    }
+}
