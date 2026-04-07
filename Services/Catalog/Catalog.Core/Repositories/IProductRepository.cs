ca9cf5c0049d94b72dd1102160b8db93f4b3483f
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-07 14:34:58 +0200

    feat: add catalog product repositories

---
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(string id);
+        Task<IEnumerable<Product>> GetAllByName(string name);
+        Task<IEnumerable<Product>> GetAllByBrand(string name);
+        Task<Product> CreateAsync(Product product);
+        Task<bool> UpdateAsync(Product product);
+        Task DeleteAsync(string id);
+    }
+}
