264a69373ea15cfc3c8bebe6959ddf2f6dc245c4
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-08 11:12:19 +0200

    feat: basket add implementation redies repository

---
+++ Services/Basket/Basket.Core/Repositories/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.Core.Entities;
+
+namespace Basket.Core.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasketAsync(string userName);
+        Task<ShoppingCart> UpdateBasketAsync(ShoppingCart cart);
+        Task DeleteBasket(string userName);
+    }
+}
---
+++ Services/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
@@ -0,0 +1,47 @@
+﻿using Basket.Core.Entities;
+using Basket.Core.Repositories;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.Infrastructure.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        public async Task<ShoppingCart> GetBasketAsync(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+        public async Task<ShoppingCart> UpdateBasketAsync(ShoppingCart cart)
+        {
+            var basket = await _redisCache.GetStringAsync(cart.UserName);
+
+            if (string.IsNullOrEmpty(basket))
+            {
+                // logic return
+            }
+
+            await _redisCache.SetStringAsync(cart.UserName, JsonConvert.SerializeObject(cart));
+            return await GetBasketAsync(cart.UserName);
+        }
+
+        public Task DeleteBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
