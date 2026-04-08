4a877fd8dd2747b16c27d93328eacee6b72eb1c5
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-08 07:46:49 +0200

    feat: catalog microservice add create product endpoint

--- Services/Catalog/Catalog.API/Controllers/ProductController.cs
+++ Services/Catalog/Catalog.API/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Catalog.Application.Queries;
+﻿using Catalog.Application.Commands;
+using Catalog.Application.Queries;
 using Catalog.Application.Responses;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,7 +11,6 @@ namespace Catalog.API.Controllers
         [HttpGet]
         [Route("[action]/{id}", Name = "GetProductById")]
         [ProducesResponseType(typeof(ProductResponseDto), StatusCodes.Status200OK)]
-        [ProducesResponseType((int)StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductResponseDto>> GetProductById(string id)
         {
             var query = new GetProductByIdQuery(id);
@@ -21,7 +21,6 @@ namespace Catalog.API.Controllers
         [HttpGet]
         [Route("[action]/{productName}", Name = "GetProductsByName")]
         [ProducesResponseType(typeof(IList<ProductResponseDto>), StatusCodes.Status200OK)]
-        [ProducesResponseType((int)StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IList<ProductResponseDto>>> GetProductsByName(string productName)
         {
             var query = new GetAllProductsByNameQuery(productName);
@@ -32,11 +31,19 @@ namespace Catalog.API.Controllers
         [HttpGet]
         [Route("GetAllProducts")]
         [ProducesResponseType(typeof(IList<ProductResponseDto>), StatusCodes.Status200OK)]
-        [ProducesResponseType((int)StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IList<ProductResponseDto>>> GetAllProducts()
         {
             var query = new GetAllProductsQuery();
             var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("GreateProduct")]
+        [ProducesResponseType(typeof(ProductResponseDto), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IList<ProductResponseDto>>> GreateProduct(CreateProductCommand productCommand)
+        {
+            var result = await _mediator.Send(productCommand);
             return Ok(result);
         }
     }
