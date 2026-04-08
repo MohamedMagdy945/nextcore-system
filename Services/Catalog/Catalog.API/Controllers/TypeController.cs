63b3c9c27f752952ddad247be18b3805496fe780
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-08 08:24:50 +0200

    feat: catalog microservice add product type endpoint

--- Services/Catalog/Catalog.API/Controllers/TypeController.cs
+++ Services/Catalog/Catalog.API/Controllers/TypeController.cs
@@ -1,6 +1,19 @@
-﻿namespace Catalog.API.Controllers
+﻿using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
 {
-    public class TypeController
+    public class TypeController : BaseApiController
     {
+        [HttpGet]
+        [Route("GetAllTypes")]
+        [ProducesResponseType(typeof(IList<BrandResponseDto>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IList<ProductResponseDto>>> GetAllTypes()
+        {
+            var query = new GetAllProductsQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
     }
 }
