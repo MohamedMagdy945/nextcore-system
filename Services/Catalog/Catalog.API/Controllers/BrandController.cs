811d0c99187b6b28dd8fe9a9f6109a0c5927137e
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-08 08:24:22 +0200

    feat: catalog microservice add product brand endpoint

---
+++ Services/Catalog/Catalog.API/Controllers/BrandController.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+
+    public class BrandController : BaseApiController
+    {
+        [HttpGet]
+        [Route("GetAllBrands")]
+        [ProducesResponseType(typeof(IList<BrandResponseDto>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IList<ProductResponseDto>>> GetAllBrands()
+        {
+            var query = new GetAllProductsQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
