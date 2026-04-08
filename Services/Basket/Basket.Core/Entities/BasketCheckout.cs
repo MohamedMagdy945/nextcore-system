5aa685e201fd4cbabc9f82da20af2dc673b7233d
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-08 10:37:56 +0200

    feat: basket and core entities

---
+++ Services/Basket/Basket.Core/Entities/BasketCheckout.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.Core.Entities
+{
+    public class BasketCheckout
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string TotalPrice { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+        public string CardName { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string Expiration { get; set; } = string.Empty;
+        public string CVV { get; set; } = string.Empty;
+        public int PaymentMethod { get; set; }
+    }
+}
