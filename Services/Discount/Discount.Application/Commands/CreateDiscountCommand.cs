66e5056d9be1af1bf73b1dbbcf860546d5874d6a
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-12 16:57:25 +0200

    feat(Discount-Application): add create discount command and handler

---
+++ Services/Discount/Discount.Application/Commands/CreateDiscountCommand.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Grpc.Protos;
+using MediatR;
+
+namespace Discount.Application.Commands
+{
+    public class CreateDiscountCommand : IRequest<CouponModel>
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public string Discription { get; set; } = string.Empty;
+        public int Amount { get; set; }
+    }
+}
