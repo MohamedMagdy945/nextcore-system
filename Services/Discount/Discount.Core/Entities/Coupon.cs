1bc211c960ac04ac71cbca591df5ddbed37f73d6
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-12 13:21:54 +0200

    feat(Discount): add coupun entities and inerface repo

---
+++ Services/Discount/Discount.Core/Entities/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Discount.Core.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Amount { get; set; }
+    }
+}
