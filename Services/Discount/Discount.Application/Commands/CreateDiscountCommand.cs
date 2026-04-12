16c6cd1d5aefd3cc6dd95d3aea9d3c216398620e
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-12 18:08:46 +0200

    feat(Discount-Application): add discount CRUD service API

--- Services/Discount/Discount.Application/Commands/CreateDiscountCommand.cs
+++ Services/Discount/Discount.Application/Commands/CreateDiscountCommand.cs
@@ -6,7 +6,7 @@ namespace Discount.Application.Commands
     public class CreateDiscountCommand : IRequest<CouponModel>
     {
         public string ProductName { get; set; } = string.Empty;
-        public string Discription { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public int Amount { get; set; }
     }
 }
