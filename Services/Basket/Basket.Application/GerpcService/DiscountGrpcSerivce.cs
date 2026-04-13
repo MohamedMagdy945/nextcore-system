31987f461ea4bb04876b5210883a479530d98209
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-13 16:24:48 +0200

    chor(Basket-Application): connect with discount grpc service proto file

---
+++ Services/Basket/Basket.Application/GerpcService/DiscountGrpcSerivce.cs
@@ -0,0 +1,23 @@
+﻿
+using Discount.Grpc.Protos;
+
+namespace Basket.Application.GerpcService
+{
+    public class DiscountGrpcSerivce
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountGrpcClient;
+
+        public DiscountGrpcSerivce(DiscountProtoService.DiscountProtoServiceClient discountGrpcClient)
+        {
+            _discountGrpcClient = discountGrpcClient;
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var request = new GetDiscountRequest { ProductName = productName };
+            var response = await _discountGrpcClient.GetDiscountAsync(request);
+            return response;
+        }
+
+    }
+}
