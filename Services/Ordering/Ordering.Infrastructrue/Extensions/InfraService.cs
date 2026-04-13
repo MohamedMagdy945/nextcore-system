5fb5b8a4af72f6426885daa9ec8d4bb8dc8da3bb
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-13 16:24:48 +0200

    feat(order-infrastructure): add register service

---
+++ Services/Ordering/Ordering.Infrastructrue/Extensions/InfraService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructrue.Data;
+using Ordering.Infrastructrue.Repositories;
+
+namespace Ordering.Infrastructrue.Extensions
+{
+    public static class InfraService
+    {
+        public static void AddInfraService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString"),
+                sqlServerOptions => sqlServerOptions.EnableRetryOnFailure())
+                );
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+        }
+    }
+}
