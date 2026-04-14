59fafef1176896265e7271ff491ac298d0a97b8f
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-14 18:12:03 +0200

    feat(order-infrastructure): add order context class

---
+++ Services/Ordering/Ordering.Infrastructrue/Data/OrderContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructrue.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public Task<int> SaveEntitiesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entity in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entity.State)
+                {
+                    case EntityState.Added:
+                        entity.Entity.CreatedDate = DateTime.UtcNow;
+                        entity.Entity.CreatedBy = "Abanoub";
+                        break;
+                    case EntityState.Modified:
+                        entity.Entity.LastModifiedBy = "Abanoub";
+                        entity.Entity.LastModifiedDate = DateTime.UtcNow;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
