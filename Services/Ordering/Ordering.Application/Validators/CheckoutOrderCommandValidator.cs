cd6d18d754479c60e8b51e9f4e4c90a76ae451ce
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-15 10:59:09 +0200

    feat(order-application): add create order command validation

---
+++ Services/Ordering/Ordering.Application/Validators/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Ordering.Application.Commands;
+
+namespace Ordering.Application.Validators
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty().WithMessage("UserName is required.")
+                .MaximumLength(50).WithMessage("UserName must not exceed 50 characters.");
+
+            RuleFor(x => x.EmailAddress)
+                .NotEmpty().WithMessage("EmailAddress is required.")
+                .EmailAddress().WithMessage("EmailAddress must be a valid email format.");
+
+            RuleFor(x => x.TotalPrice)
+                .GreaterThan(0).WithMessage("TotalPrice must be greater than zero.");
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("FirstName is required.")
+                .MaximumLength(50).WithMessage("FirstName must not exceed 50 characters.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("LastName is required.")
+                .MaximumLength(50).WithMessage("LastName must not exceed 50 characters.");
+        }
+    }
+}
