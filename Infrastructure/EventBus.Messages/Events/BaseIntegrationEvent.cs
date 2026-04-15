0c0cd2454419860c0b7871373167f7eb217ce4dd
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-15 20:55:25 +0200

    feat(eventbus): add order event bus

---
+++ Infrastructure/EventBus.Messages/Events/BaseIntegrationEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace EventBus.Messages.Events
+{
+    public class BaseIntegrationEvent
+    {
+        public string CorrelationId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public BaseIntegrationEvent()
+        {
+            CorrelationId = Guid.NewGuid().ToString();
+            CreationDate = DateTime.UtcNow;
+        }
+        public BaseIntegrationEvent(string correlationId, DateTime creationDate)
+        {
+            CorrelationId = correlationId;
+            CreationDate = creationDate;
+        }
+
+    }
+}
