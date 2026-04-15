3fbfc75cea614eb6ef37ac928bbb9a092d199c79
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-15 13:55:39 +0200

    feat(order-api): add ordercontroller add get list of order endpoint

---
+++ Services/Ordering/Ordering.API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.API.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator? _mediatorInstance;
+        protected IMediator _mediator => _mediatorInstance ??= HttpContext.RequestServices.GetService<IMediator>()!;
+
+    }
+}
---
+++ Services/Ordering/Ordering.API/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Responses;
+
+namespace Ordering.API.Controllers
+{
+    public class OrdersController : BaseApiController
+    {
+        private readonly ILogger<OrdersController> _logger;
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{userName}", Name = "GetOrdersByUserName")]
+        [ProducesResponseType(typeof(IEnumerable<OrderResponse>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<OrderResponse>>> GetOrdersByUserName(string userName)
+        {
+            var query = new GetOrdersListQuery(userName);
+            var orders = await _mediator.Send(query);
+            return Ok(orders);
+        }
+
+    }
+}
