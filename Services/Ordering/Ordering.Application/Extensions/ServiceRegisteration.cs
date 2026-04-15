fd63c96e7ec6e7971b62334738a686d18faa1bfc
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-15 12:04:26 +0200

    feat(order-application): add application service registeration

---
+++ Services/Ordering/Ordering.Application/Extensions/ServiceRegisteration.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviors;
+using System.Reflection;
+
+namespace Ordering.Application.Extensions
+{
+    public static class ServiceRegisteration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            return services;
+        }
+
+    }
+}
