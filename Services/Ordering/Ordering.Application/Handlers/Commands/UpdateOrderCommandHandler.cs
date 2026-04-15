7eed1141dcc1891745e149828db53c378e1fa0f6
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-15 11:12:30 +0200

    feat(order-application): add update order command and handler

---
+++ Services/Ordering/Ordering.Application/Handlers/Commands/UpdateOrderCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Commands;
+using Ordering.Application.Exceptions;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Handlers.Commands
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand, Unit>
+    {
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+        private readonly IMapper _mapper;
+        private readonly IOrderRepository _orderRepository;
+        public UpdateOrderCommandHandler(
+            ILogger<UpdateOrderCommandHandler> logger,
+            IMapper mapper,
+            IOrderRepository orderRepository)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await _orderRepository.GetByIdAsync(request.Id);
+
+            if (orderToUpdate == null)
+            {
+                _logger.LogError($"Order with id {request.Id} is not found.");
+                throw new OrderNotFoundException(nameof(Order), request.Id);
+            }
+
+            var orderEntity = _mapper.Map(request, orderToUpdate);
+
+            await _orderRepository.UpdateAsync(orderEntity);
+            return Unit.Value;
+        }
+
+    }
+}
