c2179d10f6454efe567f3d001219ea738a0b18ac
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-15 11:31:20 +0200

    feat(order-application): add unhandled exception behavoir

---
+++ Services/Ordering/Ordering.Application/Behaviors/UnhandledExceptionBehavior.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Behaviors
+{
+    public class UnhandledExceptionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Unhandled exception for request {RequestName} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
