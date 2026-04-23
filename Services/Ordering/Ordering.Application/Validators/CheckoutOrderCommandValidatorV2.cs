ad3ead71c9efaa0c6e269fecfcab93e032ead508
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-23 18:26:15 +0200

    feat-order: create checkout version2 command and validator

---
+++ Services/Ordering/Ordering.Application/Validators/CheckoutOrderCommandValidatorV2.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Ordering.Application.Commands;
+
+namespace Ordering.Application.Validators
+{
+    public class CheckoutOrderCommandValidatorV2 : AbstractValidator<CheckoutOrderCommadV2>
+    {
+        public CheckoutOrderCommandValidatorV2()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty().WithMessage("UserName is required.")
+                .MaximumLength(50).WithMessage("UserName must not exceed 50 characters.");
+
+            RuleFor(x => x.TotalPrice)
+                .GreaterThan(0).WithMessage("TotalPrice must be greater than zero.");
+        }
+    }
+}
