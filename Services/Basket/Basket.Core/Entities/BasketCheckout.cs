a97ac72b50caca07b303a6572223d2d892d0a1aa
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-11 15:29:05 +0300

    feat-basket: add country entities

--- Services/Basket/Basket.Core/Entities/BasketCheckout.cs
+++ Services/Basket/Basket.Core/Entities/BasketCheckout.cs
@@ -3,12 +3,13 @@
     public class BasketCheckout
     {
         public string UserName { get; set; } = string.Empty;
-        public double TotalPrice { get; set; }
+        public decimal TotalPrice { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string AddressLine { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
         public string ZipCode { get; set; } = string.Empty;
         public string CardName { get; set; } = string.Empty;
