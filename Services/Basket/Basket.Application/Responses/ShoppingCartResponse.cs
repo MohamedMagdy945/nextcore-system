e734127373140fed6c33b6b3f9785d3f99ecca89
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-12 18:46:43 +0300

    feat-basket: covert price from shopping cart item to decimal

--- Services/Basket/Basket.Application/Responses/ShoppingCartResponse.cs
+++ Services/Basket/Basket.Application/Responses/ShoppingCartResponse.cs
@@ -11,11 +11,11 @@ namespace Basket.Application.Responses
         {
             UserName = userName;
         }
-        public double TotalPrice
+        public decimal TotalPrice
         {
             get
             {
-                double total = 0;
+                decimal total = 0;
                 foreach (var item in Items)
                 {
                     total += item.Price * item.Quantity;
