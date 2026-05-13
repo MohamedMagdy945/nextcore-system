f0896188e0bca66274ce15f23502ebf14b20aa90
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-13 09:45:22 +0300

    refactor-basket: convert command and command handler in one folder

---
+++ Services/Basket/Basket.Application/Features/Commands/DeleteShoppingCartByUserName/DeleteShoppingCarttByUsetNameCommand.cs
@@ -0,0 +1,25 @@
+﻿using Basket.Core.Repositories;
+using MediatR;
+
+namespace Basket.Application.Features.Commands.DeleteShoppingCartByUserName
+{
+    public record class DeleteShoppingCartByUserNameCommand(string UserName)
+        : IRequest<Unit>;
+    public class DeleteShoppingCartByUserNameHandler
+        : IRequestHandler<DeleteShoppingCartByUserNameCommand, Unit>
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public DeleteShoppingCartByUserNameHandler(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteShoppingCartByUserNameCommand request, CancellationToken cancellationToken)
+        {
+            await _basketRepository.DeleteCartAsync(request.UserName);
+            return Unit.Value;
+        }
+    }
+
+}
