ed67ffce53df3ef7fbc789fff89f75a2d65f03c2
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-17 11:21:51 +0300

    refactor:order: edit file pathes of getordersbyusername query

---
+++ Services/Ordering/Ordering.Application/Features/Queries/GetOrdersByUserName/GetOrdersByUserNameQuery.cs
@@ -0,0 +1,24 @@
+﻿using Mapster;
+using MediatR;
+using Ordering.Application.Responses;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Features.Queries.GetOrdersByUserName
+{
+    public record GetOrdersByUserNameQuery(string UserName) : IRequest<IList<OrderResponse>>;
+    public class GetOrdersByUserNameHandler : IRequestHandler<GetOrdersByUserNameQuery, IList<OrderResponse>>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public GetOrdersByUserNameHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<IList<OrderResponse>> Handle(GetOrdersByUserNameQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersByUserNameAsync(request.UserName);
+            return orderList.Adapt<IList<OrderResponse>>();
+        }
+    }
+}
