d858530c3697fb9bd04541e70085c1097eaef71d
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-18 16:39:25 +0300

    fix-order: change record entities

--- Services/Ordering/Ordering.Application/Features/Commands/UpdateOrder/UpdateOrderCommand.cs
+++ Services/Ordering/Ordering.Application/Features/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -2,21 +2,22 @@
 
 namespace Ordering.Application.Features.Commands.UpdateOrder
 {
-    public record UpdateOrderCommand(
-        int Id,
-        string UserName,
-        decimal TotalPrice,
-        string FirstName,
-        string LastName,
-        string AddressLine,
-        string Email,
-        string Country,
-        string State,
-        string ZipCode,
-        string CardName,
-        string CardNumber,
-        string Expiration,
-        string CVV,
-        int PaymentMethod
-  ) : IRequest<Unit>;
+    public record UpdateOrderCommand : IRequest
+    {
+        public int Id { get; init; }
+        public string UserName { get; init; } = string.Empty;
+        public decimal TotalPrice { get; init; }
+        public string FirstName { get; init; } = string.Empty;
+        public string LastName { get; init; } = string.Empty;
+        public string AddressLine { get; init; } = string.Empty;
+        public string Email { get; init; } = string.Empty;
+        public string Country { get; init; } = string.Empty;
+        public string State { get; init; } = string.Empty;
+        public string ZipCode { get; init; } = string.Empty;
+        public string CardName { get; init; } = string.Empty;
+        public string CardNumber { get; init; } = string.Empty;
+        public string Expiration { get; init; } = string.Empty;
+        public string CVV { get; init; } = string.Empty;
+        public int PaymentMethod { get; init; }
+    }
 }
