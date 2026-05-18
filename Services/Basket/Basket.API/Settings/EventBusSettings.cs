6d5ad60fb060bab25d938f57f0856cde6c9ddbbc
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-18 12:25:49 +0300

    refactor-basket: configure rabbitMQ

---
+++ Services/Basket/Basket.API/Settings/EventBusSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.API.Settings
+{
+    public class EventBusSettings
+    {
+        public string Host { get; set; } = string.Empty;
+        public int Port { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
