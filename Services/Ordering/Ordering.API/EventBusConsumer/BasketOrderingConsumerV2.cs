1cfc1f3ecab1a05136155bb8b21e5261c3129c01
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-21 17:50:57 +0300

    feat-order: add ckeckoutv2 consumer

---
+++ Services/Ordering/Ordering.API/EventBusConsumer/BasketOrderingConsumerV2.cs
@@ -0,0 +1,37 @@
+﻿using EventBus.Messages.Events;
+using Mapster;
+using MassTransit;
+using MediatR;
+using Ordering.Application.Features.Commands.CheckoutOrderV2;
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketOrderingConsumerV2 : IConsumer<BasketCheckoutEventV2>
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<BasketOrderingConsumerV2> _logger;
+        public BasketOrderingConsumerV2(IMediator mediator,
+            ILogger<BasketOrderingConsumerV2> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEventV2> context)
+        {
+            var message = context.Message;
+
+            using var scope = _logger.BeginScope(
+                "Consume BasketCheckoutEvent - CorrelationId: {CorrelationId}",
+                message.CorrelationId);
+
+            var cmd = message.Adapt<CheckoutOrderCommandV2>();
+
+            var result = await _mediator.Send(cmd);
+
+            _logger.LogInformation(
+                "BasketCheckoutEvent consumed successfully for User: {UserName}",
+                message.UserName);
+        }
+    }
+}
