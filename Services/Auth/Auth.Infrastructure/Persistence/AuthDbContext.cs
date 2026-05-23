f86994d68100ca92355e8e52f1ccc5aa84726d4b
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-23 08:50:33 +0300

    feat-Infrastructure: add Auth Db context

--- Services/Auth/Auth.Infrastructure/Persistence/AuthDbContext.cs
+++ Services/Auth/Auth.Infrastructure/Persistence/AuthDbContext.cs
@@ -1,6 +1,11 @@
-﻿namespace Auth.Infrastructure.Persistence
+﻿using Auth.Application.Interfaces;
+using Auth.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Auth.Infrastructure.Persistence
 {
-    public class AuthDbContext
+    public class AuthDbContext : IdentityDbContext
+        <ApplicationUser, ApplicationRole, int>, IAuthDbContext
     {
     }
 }
