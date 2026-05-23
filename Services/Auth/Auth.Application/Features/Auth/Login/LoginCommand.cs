71d89aadccc653d73c9f5ec9e64cb54f9f1a25c4
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-23 09:30:03 +0300

    feat-application: add mediatr and login command handler

---
+++ Services/Auth/Auth.Application/Features/Auth/Login/LoginCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Auth.Application.Features.Auth.Login
+{
+    public class LoginCommand
+    {
+    }
+}
---
+++ Services/Auth/Auth.Application/Features/Auth/Login/LoginCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Auth.Application.Resources;
+using MediatR;
+using Microsoft.Extensions.Localization;
+
+namespace Auth.Application.Features.Auth.Login
+{
+    public class LoginCommandHandler // : IRequestHandler<LoginCommand, AuthResult>
+    {
+        private readonly IStringLocalizer<AuthSharedResource> _localizer;
+
+        public LoginCommandHandler(IStringLocalizer<AuthSharedResource> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public async Task<Unit> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            // فرضاً بنعمل تشيك على المستخدم وموجودش في الـ Database
+            bool userExists = false;
+
+            if (!userExists)
+            {
+                string errorMessage = _localizer["UserNotFound"];
+
+                throw new Exception(errorMessage);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
