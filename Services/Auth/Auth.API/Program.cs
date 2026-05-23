a00bf4a50f59b090c2369ff2bf09a12f729e8efd
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-23 09:26:27 +0300

    feat-api: register localization folder

--- Services/Auth/Auth.API/Program.cs
+++ Services/Auth/Auth.API/Program.cs
@@ -1,3 +1,6 @@
+using Auth.Application.Resources;
+using System.Reflection;
+
 namespace Auth.API
 {
     public class Program
@@ -6,13 +9,31 @@ namespace Auth.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            builder.Services.AddControllers()
+                .AddDataAnnotationsLocalization(options =>
+                {
+                    options.DataAnnotationLocalizerProvider = (type, factory) =>
+                    {
+                        var assemblyName = new AssemblyName(typeof(AuthSharedResource).Assembly.FullName!);
+                        return factory.Create("AuthSharedResource", assemblyName.Name!);
+                    };
+                });
+
             // Add services to the container.
 
             builder.Services.AddControllers();
 
             var app = builder.Build();
+            var supportedCultures = new[] { "en", "ar" };
 
-            // Configure the HTTP request pipeline.
+            var localizationOptions = new RequestLocalizationOptions()
+                .SetDefaultCulture(supportedCultures[0])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+
+            app.UseRequestLocalization(localizationOptions);
 
             app.UseAuthorization();
 
