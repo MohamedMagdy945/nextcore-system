e467b0190970fa320283b468ff90594c711d3661
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-24 10:35:49 +0300

    feat-auth: add base identity entity

---
+++ Services/Auth/Auth.Infrastructure/Entities/BaseIdentityEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Auth.Infrastructure.Entities
+{
+    internal class BaseIdentityEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+    }
+}
---
+++ Services/Auth/Auth.Infrastructure/Entities/Permission.cs
@@ -0,0 +1,8 @@
+﻿namespace Auth.Infrastructure.Entities
+{
+    internal class Permission
+    {
+        public string Name { get; set; } = null!;
+        //public ICollection<RolePermission> RolePermissions { get; set; } = new List<RolePermission>();
+    }
+}
