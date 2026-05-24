5c711cc214d7ab400c03cb258dccc07d60ffa145
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-24 10:52:54 +0300

    feat-auth: RolePermissionConfiguration

---
+++ Services/Auth/Auth.Infrastructure/Configuration/ApplicationUserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Auth.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auth.Infrastructure.Configuration
+{
+    public class ApplicationUserConfiguration :
+        IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.HasIndex(x => x.Email)
+                .IsUnique();
+        }
+    }
+}
---
+++ Services/Auth/Auth.Infrastructure/Configuration/RolePermissionConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Auth.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auth.Infrastructure.Configuration
+{
+    public class RolePermissionConfiguration
+      : IEntityTypeConfiguration<RolePermission>
+    {
+        public void Configure(EntityTypeBuilder<RolePermission> builder)
+        {
+
+            builder.HasKey(x => new
+            {
+                x.RoleId,
+                x.PermissionId
+            });
+
+            builder.HasOne(x => x.Role)
+                .WithMany(x => x.RolePermissions)
+                .HasForeignKey(x => x.RoleId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Permission)
+                .WithMany(x => x.RolePermissions)
+                .HasForeignKey(x => x.PermissionId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
