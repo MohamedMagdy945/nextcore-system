33b53e087279114ccb859d6915bc5e222218f721
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-24 11:06:57 +0300

    feat-auth:add  UserRoleConfiguration

---
+++ Services/Auth/Auth.Infrastructure/Configuration/UserRoleConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Auth.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auth.Infrastructure.Configuration
+{
+    public class UserRoleConfiguration
+        : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+
+            builder.HasKey(x => new
+            {
+                x.UserId,
+                x.RoleId
+            });
+
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.UserRoles)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Role)
+                .WithMany(x => x.UserRoles)
+                .HasForeignKey(x => x.RoleId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
