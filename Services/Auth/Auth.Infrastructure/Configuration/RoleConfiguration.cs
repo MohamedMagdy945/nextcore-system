b25e9ef093e27ead26a54820b53e56abd66b6a27
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-24 11:12:06 +0300

    feat-auth:add  Role configuartion

---
+++ Services/Auth/Auth.Infrastructure/Configuration/RoleConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Auth.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auth.Infrastructure.Configuration
+{
+    public class RoleConfiguration
+        : IEntityTypeConfiguration<ApplicationRole>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationRole> builder)
+        {
+
+            builder.HasIndex(x => x.Name)
+                .IsUnique();
+        }
+    }
+}
