6f1e55988a07c64c021cadba7b85961582c2dfff
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-24 12:01:24 +0300

    recator-auth: edit application role

--- Services/Auth/Auth.Infrastructure/Entities/ApplicationRole.cs
+++ Services/Auth/Auth.Infrastructure/Entities/ApplicationRole.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Auth.Infrastructure.Entities
+﻿namespace Auth.Infrastructure.Entities
 {
-    public class ApplicationRole : IdentityRole<int>
+    public class ApplicationRole : BaseIdentityEntity
     {
-        public ICollection<RolePermission> RolePermissions { get; private set; }
-              = new List<RolePermission>();
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
 
-        public ICollection<UserRole> UserRoles { get; private set; }
-            = new List<UserRole>();
+        public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
+        public ICollection<RolePermission> RolePermissions { get; set; } = new List<RolePermission>();
     }
 }
