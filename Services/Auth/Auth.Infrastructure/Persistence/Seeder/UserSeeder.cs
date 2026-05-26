fa90928ef217e3a1af58e1803f09d220f2c5e6e2
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-26 11:28:03 +0300

    feat-auth: add user seeder

---
+++ Services/Auth/Auth.Infrastructure/Persistence/Seeder/UserSeeder.cs
@@ -0,0 +1,80 @@
+﻿using Auth.Application.Interfaces;
+using Auth.Domain.Entities;
+using Common.Shared.Constant;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Infrastructure.Persistence.Seeder;
+
+public class UserSeeder
+{
+    private readonly IAuthDbContext _context;
+    private readonly IPasswordHasher _passwordHasher;
+
+    public UserSeeder(
+        IAuthDbContext context,
+        IPasswordHasher passwordHasher)
+    {
+        _context = context;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task SeedAsync()
+    {
+        if (await _context.Users.AnyAsync())
+        {
+            return;
+        }
+
+        var adminRole = await _context.Roles
+            .FirstOrDefaultAsync(r => r.Name == Roles.Admin);
+
+        var userRole = await _context.Roles
+            .FirstOrDefaultAsync(r => r.Name == Roles.User);
+
+        if (adminRole is null || userRole is null)
+        {
+            return;
+        }
+
+        var adminUser = new User
+        {
+            FirstName = "System",
+            LastName = "Admin",
+            UserName = "admin",
+            Email = "admin@gmail.com",
+            PasswordHash = _passwordHasher.Hash("Admin@123"),
+        };
+
+        var normalUser = new User
+        {
+            FirstName = "Test",
+            LastName = "User",
+            UserName = "user",
+            Email = "user@gmail.com",
+            PasswordHash = _passwordHasher.Hash("User@123"),
+        };
+
+        await _context.Users.AddRangeAsync(adminUser, normalUser);
+
+        await _context.SaveChangesAsync();
+
+        var userRoles = new List<UserRole>
+        {
+            new()
+            {
+                UserId = adminUser.Id,
+                RoleId = adminRole.Id
+            },
+
+            new()
+            {
+                UserId = normalUser.Id,
+                RoleId = userRole.Id
+            }
+        };
+
+        await _context.UserRoles.AddRangeAsync(userRoles);
+
+        await _context.SaveChangesAsync();
+    }
+}
