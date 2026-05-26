b44928bec4e821bdd485b68e9a5e9121972dbe99
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-26 11:26:20 +0300

    feat-auth: add role seeder

---
+++ Services/Auth/Auth.Infrastructure/Persistence/Seeder/RoleSeeder.cs
@@ -0,0 +1,33 @@
+﻿using Auth.Application.Interfaces;
+using Auth.Domain.Entities;
+using Common.Shared.Constant;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Infrastructure.Persistence.Seeder;
+
+public class RoleSeeder
+{
+    private readonly IAuthDbContext _context;
+
+    public RoleSeeder(IAuthDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SeedAsync()
+    {
+        if (await _context.Roles.AnyAsync())
+        {
+            return;
+        }
+        var roles = Roles.GetAll();
+
+        var rolesToDb = roles.Select(r => new Role { Name = r }).ToList();
+
+
+
+        await _context.Roles.AddRangeAsync(rolesToDb);
+
+        await _context.SaveChangesAsync();
+    }
+}
