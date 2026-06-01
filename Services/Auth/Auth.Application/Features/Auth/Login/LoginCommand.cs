6bf45e734f26d909275b8d9eb6deff4e3507ce87
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-06-01 09:09:31 +0300

    feat-auth: add login command entities

--- Services/Auth/Auth.Application/Features/Auth/Login/LoginCommand.cs
+++ Services/Auth/Auth.Application/Features/Auth/Login/LoginCommand.cs
@@ -1,9 +1,12 @@
 ﻿using Auth.Application.Bases;
+using Auth.Application.Common;
 using MediatR;
 
 namespace Auth.Application.Features.Auth.Login
 {
-    public class LoginCommand : IRequest<Result<Unit>>
+    public record LoginCommand : IRequest<Result<TokenResponse>>
     {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
---
+++ Services/Auth/Auth.Application/Features/Auth/Login/LoginCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Auth.Application.Features.Auth.Login
+{
+    public class LoginCommandValidator : AbstractValidator<LoginCommand>
+    {
+        public LoginCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email format");
+
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters");
+
+        }
+    }
+}
