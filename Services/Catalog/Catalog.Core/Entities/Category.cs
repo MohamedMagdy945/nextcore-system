be6a815a7188f06ff158a96b7733c6da77d83ddf
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-06-07 16:23:29 +0300

    feat: add category image url and slog entities

--- Services/Catalog/Catalog.Core/Entities/Category.cs
+++ Services/Catalog/Catalog.Core/Entities/Category.cs
@@ -3,5 +3,7 @@
     public class Category : BaseEntity
     {
         public string Name { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
     }
 }
